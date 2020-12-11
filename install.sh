46b56d9eea4f6f2e2eeea294bed27c55e3708263
Termux <70668507+TermuxHackz@users.noreply.github.com>
2020-12-11 15:59:45 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#installation
+
+clear
+
+echo ""
+echo ""
+echo " SUBSCRIBE MY CHANNEL ITS FOR GAMES BUT ILL CREATE ONE FOR HACKING SOON🔔 " | lolcat
+xdg-open https://youtu.be/nIbfy20KGv0
+echo ""
+
+echo -e "\e[101m Press Enter \e[0m"
+read a1
+
+sleep 1
+clear
+
+echo "Loading..." | lolcat
+sleep 1
+clear
+
+echo "Loading..." | lolcat
+sleep 1
+clear
+
+echo "Loading..." | lolcat
+sleep 1
+clear
+
+toilet -f future "AnonyminHack5" | lolcat
+echo "-------------------------------------------------------" | lolcat
+echo   "Created  : ANONYMINHACK5 and Contributions to Ultrakid⁩ and Elisha⁩ $white" |lolcat
+echo   "Contact  : facebook.com/anonyminHack5 $white" |lolcat
+echo   "Group  : Termux Android Hackers $white" |lolcat
+echo "-------------------------------------------------------" | lolcat
+
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  "Detected, Trying To Exit  ... "
+echo ""
+sleep 1
+echo ""
+echo "ANONYMIN-HACK5" | lolcat
+sleep 1
+exit
+}
+
+apt update
+apt upgrade -y
+pkg install ruby -y 
+gem install toilet -y 
+pkg install lolcat
+pkg install figlet
+apt install git php openssh curl -y
+pkg install python2 -y 
+pkg install nano php -y
+pip2 install mechanize
+pip2 install --upgrade pip
+
+echo ""
+echo "CODE_BY_ANONYMIN_HACK5" | lolcat
+echo "________________________" | lolcat
---
+++ tools.sh
@@ -0,0 +1,289 @@
+#!/bin/bash
+# version 2.0
+
+clear
+
+
+echo ""
+echo -e "SUBSCRIBE MY CHANNEL"
+echo ""
+
+echo -e "\e[101m Press Enter \e[0m"
+read a1
+
+echo "Loading..." | lolcat
+sleep 2
+clear
+
+toilet -f future "ANONYMINHACK5" | lolcat
+echo "-------------------------------------------------------" | lolcat
+echo   "Author   : ANONYMINHACK5, ULTRAKID AND ELISHA $white" |lolcat
+echo   "Contact  : https://facebook.com/anonyminHack5 $white" |lolcat
+echo   "YouTube  : GAMER LINKS $white" |lolcat
+echo   "Blog     : https://termuxhack.simdif.com $white" |lolcat
+echo "-------------------------------------------------------" | lolcat
+echo "                           v2.0              " | lolcat
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  "Detected, Trying To Exit 🚪 ... "
+echo ""
+echo  "⚠️NOT FOR ILLEGEL USE⚠️"
+sleep 1
+echo ""
+echo "ANONYMINHACK5" | lolcat
+echo ""
+echo "BYE BYE 👋👋..." | lolcat
+echo ""
+echo "COME BACK 🔜" | lolcat
+sleep 1
+exit
+}
+
+lagi=01
+while [ $lagi -lt 15 ];
+do
+echo ""
+echo -e "\e[1;101m\e[1;97m01\e[1;101m\e[0m\e[1;96m TOOL-X\e[0m\n";
+echo -e "\e[1;101m\e[1;97m02\e[1;101m\e[0m\e[1;96m LUCIFER\e[0m\n";
+echo -e "\e[1;101m\e[1;97m03\e[1;101m\e[0m\e[1;96m WEEMAN\e[0m\n";
+echo -e "\e[1;101m\e[1;97m04\e[1;101m\e[0m\e[1;96m RED_HAWK\e[0m\n";
+echo -e "\e[1;101m\e[1;97m05\e[1;101m\e[0m\e[1;96m TECH-HACH\e[0m\n";
+echo -e "\e[1;101m\e[1;97m06\e[1;101m\e[0m\e[1;96m BRUTEFORCE_FACEBOOK\e[0m\n";
+echo -e "\e[1;101m\e[1;97m07\e[1;101m\e[0m\e[1;96m BOT_KONEN_FACEBOOK\e[0m\n";
+echo -e "\e[1;101m\e[1;97m08\e[1;101m\e[0m\e[1;96m TERMUX-BASIC-PACKAGE\e[0m\n";
+echo -e "\e[1;101m\e[1;97m09\e[1;101m\e[0m\e[1;96m OSIF\e[0m\n";
+echo -e "\e[1;101m\e[1;97m10\e[1;101m\e[0m\e[1;96m INSTAGRAM-BRUTEFORCE\e[0m\n";
+echo -e "\e[1;101m\e[1;97m11\e[1;101m\e[0m\e[1;96m FIRECRACK\e[0m\n";
+echo -e "\e[1;101m\e[1;97m12\e[1;101m\e[0m\e[1;96m EVIL-EYE\e[0m\n";
+echo -e "\e[1;101m\e[1;97m13\e[1;101m\e[0m\e[1;96m TECH-CRASH\e[0m\n";
+echo -e "\e[1;101m\e[1;97m14\e[1;101m\e[0m\e[1;96m SPEED-X\e[0m\n";
+echo -e "\e[1;101m\e[1;97m15\e[1;101m\e[0m\e[1;96m SAYCHEESE\e[0m\n";
+echo -e "\e[1;101m\e[1;97m00\e[1;101m\e[0m\e[1;96m EXIT\e[0m\n";
+echo ""
+echo  "╭─ANONYMINHACK5" |lolcat
+read -p "╰──►" pil;
+
+
+#TOOL-X
+
+
+case $pil in
+01) clear
+toilet -f standard " ANONYMINHACK5 " -F gay
+git clone https://github.com/Rajkumrdusad/Tool-X
+cd Tool-X
+sh install.aex
+ 
+
+;;
+
+
+#LUCIFER
+
+
+02) clear
+toilet -f standard " ANONYMINHACK5 " -F gay
+git clone https://github.com/rixon-cochi/Lucifer.git
+cd Lucifer
+chmod +x *
+bash setup.sh
+ls
+bash instacracker.sh
+
+
+;;
+
+
+#WEEMAN
+
+
+03) clear
+toilet -f standard " ANONYMINHACK5 " -F gay
+git clone https://github.com/evait-security/weeman.git
+cd weeman
+python2 weeman.py
+
+
+;;
+
+
+#RED_HAWK
+
+
+04) clear
+toilet -f standard " ANONYMINHACK5 " -F gay
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+php rhawk.php
+
+
+;;
+
+
+#ANONYMINHACK5
+
+
+05) clear
+toilet -f standard " ANONYMINHACK5 " -F gay
+git clone https://github.com/rixon-cochi/tech-hack.git
+cd tech-hack
+chmod +x *
+unzip sites.zip
+bash cracker.sh
+
+
+;;
+
+
+#BRUTEFORCE_FACEBOOK
+
+
+06) clear
+toilet -f standard " ANONYMINHACK5 " -F gay
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+
+
+;;
+
+
+#BOT_KONEN_FACEBOOK
+
+
+07) clear
+toilet -f standard " ANONYMINHACK5 " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+
+;;
+
+
+#TERMUX-BASIC-PACKAGE
+
+
+08) clear
+toilet -f standard " ANONYMINHACK5 " -F gay
+https://github.com/rixon-cochi/basic-pkg
+cd basic-pkg
+bash basic.sh
+
+
+;;
+
+
+#OSIF
+
+
+09) clear
+toilet -f standard " ANONYMINHACK5 " -F gay
+git clone https://github.com/ciku370/OSIF
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+
+
+;;
+
+
+#INSTAGRAM-BRUTEFORCE
+
+
+10) clear
+toilet -f standard " ANONYMINHACK5 " -F gay
+git clone https://github.com/linuxkukeren/instagram
+pip install requests
+pip install -r requirement.txt
+pip2 install -r requirement.txt
+python2 linuxkukerenhacking.py
+
+
+;;
+
+
+#FIRECRACK
+
+
+11) clear
+toilet -f standard " ANONYMINHACK5 " -F gay
+git clone https://github.com/Ranginang67/Firecrack
+cd Firecrack
+pip2 install -r requirements.txt
+python2 firecrack.py
+
+
+;;
+
+
+#EVIL-EYE
+
+
+12) clear
+toilet -f standard " ANONYMINHACK5 " -F gay
+git clone https://github.com/rixon-cochi/Evil-eye
+Cd Evil-eye
+Chmod +x *
+bash requirement.sh
+bash Evil-l.sh
+
+
+;;
+
+
+#TECH-CRASH
+
+
+13) clear
+toilet -f standard " ANONYMINHACK " -F gay
+git clone https://github.com/rixon-cochi/techcrash
+cd techcrash
+php Whatsbot.php
+
+
+;;
+
+
+#SPEED-X
+
+
+14) clear
+toilet -f standard " ANONYMINHACK5 " -F gay
+git clone https://github.com/TheSpeedX/TBomb.git
+cd TBomb
+chmod +x TBomb.sh
+./TBomb.sh
+
+
+;;
+
+
+#SAYCHEESE
+
+
+15) clear
+toilet -f standard " ANONYMINHACK5 " -F gay
+git clone https://github.com/thelinuxchoice/saycheese
+cd saycheese
+bash saycheese.sh
+
+
+;;
+
+
+00) echo "created by : ANONYMINHACK5" | lolcat
+exit
+;;
+
+*) echo "sorry, the  option you looking is not found" | lolcat
+esac
+done
+done
+
+=================="
